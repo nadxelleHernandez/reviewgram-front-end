4eef117694fdb6b1e9b2cbe9448e2e0ee28b82d4
Soumya Sah <85212732+smysh@users.noreply.github.com>
2023-02-15 01:17:39 -0800

    Added topmovies.js to display top trending movies today.

---
+++ src/components/topmovies.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const TopMovies = (toggleShow, topMoviesData) => {
+  return <div>{/* <p>Top Movies{topMoviesData}</p> */}</div>;
+};
+
+export default TopMovies;
