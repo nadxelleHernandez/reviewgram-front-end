54c5159be3c49628b380816fb9bcdad66cc384cd
Soumya Sah <85212732+smysh@users.noreply.github.com>
2023-02-17 02:16:19 -0800

    Applied some CSS styles to main page.

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,32 +3,38 @@ import Search from "../components/search";
 import TopTVShows from "../components/toptvshows";
 import TopMovies from "../components/topmovies";
 import SearchResults from "../components/searchResults";
+import "./main.css";
 
 const Main = ({ getTopMovies, getSearchData, searchData, getTopTVShows }) => {
   return (
     <main>
       <main className="main">
-        <header>
-          <h1>ReviewGram</h1>
+        <header className="header">
+          <h1 id="AppName">ReviewGram</h1>
+
+          <section id="Search">
+            <Search createNewSearch={getSearchData}></Search>
+          </section>
         </header>
-        <section>
-          <Search createNewSearch={getSearchData}></Search>
-        </section>
-        <section>
+        <section id="SearchResults">
           <SearchResults searchData={searchData}></SearchResults>
         </section>
-        <section>
-          <TopMovies
-            toggleShow={searchData === []}
-            getTopMovies={getTopMovies}
-          ></TopMovies>
-        </section>
-        <section>
-          <TopTVShows
-            toggleShow={searchData === []}
-            getTopTVShows={getTopTVShows}
-          ></TopTVShows>
-        </section>
+        <div id="Trending">
+          <section className="Media">
+            <TopMovies
+              id="TopMovies"
+              toggleShow={searchData === []}
+              getTopMovies={getTopMovies}
+            ></TopMovies>
+          </section>
+          <section className="Media">
+            <TopTVShows
+              id="TopTVShows"
+              toggleShow={searchData === []}
+              getTopTVShows={getTopTVShows}
+            ></TopTVShows>
+          </section>
+        </div>
       </main>
     </main>
   );
