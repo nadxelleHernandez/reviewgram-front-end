709674aa3c108710766486b2d3bba1d0a5f5d328
Soumya Sah <85212732+smysh@users.noreply.github.com>
2023-02-19 22:58:01 -0800

    Basic card for top movies/ top tv/ watchlist

--- src/components/mediaentry.js
+++ src/components/mediaentry.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
 import "./mediaentry.css";
+import MediaRating from "./mediarating";
 
 const MediaEntry = ({ media }) => {
-  let media_type = "";
-  if (media.isMovie) {
-    media_type = "movie";
-  } else {
-    media_type = "tv";
-  }
-
   return (
-    <div id="title">
-      <Link to={`/${media_type}/${media.TMDB_id}`}>
-        {media.isMovie ? media.title : media.name}
-      </Link>
-      <span> Rating: {media.rating}</span>
-    </div>
+    // <div id="title">
+    //   <Link to={`/${media_type}/${media.TMDB_id}`}>
+    //     {media.isMovie ? media.title : media.name}
+    //   </Link>
+    //   <span> Rating: {media.rating}</span>
+    // </div>
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={media.poster_url} />
+      <Card.Body>
+        <Card.Title>{media.isMovie ? media.title : media.name}</Card.Title>
+        <MediaRating rating={media.rating}></MediaRating>
+      </Card.Body>
+    </Card>
   );
 };
 
