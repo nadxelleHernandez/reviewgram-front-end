7189ab3e0f48d4d1108583aef9a423f4bf4e3bf5
Soumya Sah <85212732+smysh@users.noreply.github.com>
2023-02-19 21:21:53 -0800

    Basic user watchlist is being displayed.

--- src/routes/userList.js
+++ src/routes/userList.js
@@ -2,20 +2,33 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import PropTypes from "prop-types";
+import MediaEntry from "../components/mediaentry";
 
 const UserList = ({ getUserWatchList }) => {
   const { user_id } = useParams();
   const [userWatchList, setUserWatchList] = useState([]);
 
+  const userWatchListEntries = userWatchList.map((watchlist) => {
+    return (
+      <li key={watchlist.id}>
+        <MediaEntry media={watchlist["media"]}></MediaEntry>
+      </li>
+    );
+  });
   useEffect(() => {
     getUserWatchList(user_id, "w185").then((data) => {
       console.log("In user watch list route useEffect");
-      console.log(data);
-      setUserWatchList(data);
+      console.log(data["watchlist"]);
+      setUserWatchList(data["watchlist"]);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return <main>User List</main>;
+  return (
+    <div>
+      <p>User Watch list</p>
+      <ol>{userWatchListEntries}</ol>
+    </div>
+  );
 };
 UserList.propTypes = {
   getUserWatchList: PropTypes.func.isRequired,
