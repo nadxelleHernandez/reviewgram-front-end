3f6a2c5f250ca6fcd3c08f5341df467f7e86854b
Soumya Sah <85212732+smysh@users.noreply.github.com>
2023-02-20 16:45:57 -0800

    Search results are displayed as cards within list group.

--- src/components/searchResults.js
+++ src/components/searchResults.js
@@ -1,12 +1,13 @@
 import React from "react";
 import SearchResult from "./searchresult";
+import ListGroup from "react-bootstrap/ListGroup";
 
 const SearchResults = ({ toggleShow, searchData }) => {
   const searchResults = searchData.map((entry) => {
     return (
-      <li key={entry.TMDB_id}>
+      <ListGroup.Item key={entry.TMDB_id}>
         <SearchResult media={entry}></SearchResult>
-      </li>
+      </ListGroup.Item>
     );
   });
   return <ul>{searchResults}</ul>;
